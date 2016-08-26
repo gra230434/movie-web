ce96638d8a9877625f2098d64891e97c0f7433cb
Kevin W <gra230434@gmail.com>
2016-08-26 20:38:07 +0800

    user mode

---
+++ mo-user/index.php
@@ -0,0 +1,25 @@
+<?php
+  include( dirname(__FILE__) . '/../mo-dbcon/session.php' );
+  require( dirname(__FILE__) . '/user-function.php' );
+
+  if ( !check_status( $_SESSION['login_user'] ) ) {
+    header ("location: http://movie.technologyofkevin.com/movie.php");
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <title>絕對低調</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <?php
+    admin_showuser();
+  ?>
+</body>
+</html>
---
+++ mo-user/user-function.php
@@ -0,0 +1,42 @@
+<?php
+
+  require( dirname(__FILE__) . '/../mo-dbcon/mo-config.php' );
+
+  /**
+   * check_status : check user status only let status<5
+   *
+   * @version 1.0.0
+   * @author Kevin gra230434@gmail.com
+   * @since movie system 1.2
+   */
+   function check_status( $username ){
+     $sql = "SELECT user_status FROM movie_users WHERE user_login='$username'";
+     if ( $result = mysqli_query($db,$sql) ){
+       $row = mysqli_fetch_row($result);
+       if ( $row['user_status']<5 ) {
+         return 1;
+       } else {
+         return 0;
+       }
+     }
+   }
+
+  /**
+   * user_showuser : show everyone in moviesystem
+   *
+   * @version 1.0.0
+   * @author Kevin gra230434@gmail.com
+   * @since movie system 1.2
+   */
+   function user_showuser( $username ){
+     $sql = "SELECT ID, user_login, user_lasttime, user_display FROM movie_users WHERE user_login=$username";
+     if ( $result = mysqli_query($db,$sql) ){
+       printf ("<tbody>");
+       while ( $row=mysqli_fetch_row($result) ){
+         $lastdate = empty($row['user_lasttime'])?$row['user_lasttime']:'No login';
+         printf ("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>",
+          $row['ID'],$row['user_display'],$row['user_login'],$row['user_status'],$lastdate);
+       }
+       printf ("</tbody>");
+     }
+   }
